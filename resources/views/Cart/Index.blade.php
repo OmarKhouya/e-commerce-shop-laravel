3fcf7c855ee34504ee4b8e8ee7c95d4a100025c5
Omar ka <omarkhouya66@gmail.com>
2024-04-29 00:10:07 +0100

    cart and cart-item component redesign, added total price calculation to in-cart products & full total

--- resources/views/Cart/Index.blade.php
+++ resources/views/Cart/Index.blade.php
@@ -5,18 +5,37 @@
     <section class="py-5 text-dark" style="background-color: #FEFAF6">
         <div class="container">
             <h1 class="mb-3">Cart</h1>
-            <div class="row">
-                <div class="col-lg-5">
-                    @auth
-                        @foreach ($cartItems as $item)
-                            <x-cart-items :item="$item" />
-                        @endforeach
-                    @endauth
+            @auth
+                <div class="table-responsive">
+                    <table class="table table-striped  table-hover">
+                        <thead>
+                            <tr>
+                                <th>Product</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($cartItems as $item)
+                                <x-cart-items :item="$item" />
+                            @endforeach
+                        </tbody>
+                        <tfoot>
+                            <tr class="table-active">
+                                <td colspan="3">Total</td>
+                                @php
+                                    $total = 0;
+                                    foreach ($cartItems as $item) {
+                                        $total += $item->Price * $item->pivot->quantity;
+                                    }
+                                @endphp
+                                <td>${{ $total }}</td>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>
-                <div>
-                    
-                </div>
-            </div>
+            @endauth
         </div>
     </section>
 @endSection
--- resources/views/components/cart-items.blade.php
+++ resources/views/components/cart-items.blade.php
@@ -1,13 +1,7 @@
 @props(['item'])
-<div class="px-3 border rounded border-dark my-2">
-    {{-- <a href="{{ route('products.show', $item->id) }}" class="text-decoration-none"> --}}
+{{-- <div class="px-3 border rounded border-dark my-2">
     <div class="card border-0" style="max-width: 540px;background-color: #FEFAF6">
         <div class="row row-cols-md-2 row-cols-1  g-0">
-            <div class="col">
-                {{-- <img src="{{ asset('images/uploads/' . $item->Thumbnail) }}" class="img-fluid rounded-start"
-                            alt="..."> --}}
-                <img src="{{ $item->Thumbnail }}" class="img-fluid rounded my-3" alt="...">
-            </div>
             <div class="col">
                 <div class="card-body mt-3">
                     <h5 class="card-title">{{ $item->Name }}</h5>
@@ -15,15 +9,29 @@
                     <p>
                         <small class="text-muted">Quantity: {{ $item->pivot->quantity }}</small>
                     </p>
-                    <form action="{{ route('cart.destroy', $item->id) }}" method="post" class="float-end mb-3">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit"><i
-                                class="fa-solid fa-trash"></i></button>
-                    </form>
+
                 </div>
             </div>
         </div>
     </div>
-    {{-- </a>    --}}
-</div>
+</div> --}}
+<tr>
+    <td>
+        <a href="{{ route('products.show', $item->id) }}" class="text-dark"> {{ $item->Name }}</a>
+
+    </td>
+    <td>
+        ${{ $item->Price }}
+    </td>
+    <td>
+        {{ $item->pivot->quantity }}
+    </td>
+    <td>
+        ${{ $item->pivot->quantity * $item->Price }}
+        <form action="{{ route('cart.destroy', $item->id) }}" method="post" class="float-end mb-3">
+            @csrf
+            @method('DELETE')
+            <button class="nav-link" type="submit"><i class="fa-solid fa-trash"></i></button>
+        </form>
+    </td>
+</tr>
