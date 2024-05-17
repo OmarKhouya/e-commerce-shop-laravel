3b8ce499ef59a105c6e75448a12a63c5448b397d
Omar ka <omarkhouya66@gmail.com>
2024-05-17 23:29:32 +0100

    login & register forms errors dislpayer styled using bootstrap alerts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,42 +4,30 @@
     <section class="py-5 text-dark" style="background-color: #FEFAF6">
         <div class="container">
             <h1 class="text-center">Login</h1>
+            @if ($errors)
+                @foreach ($errors->all() as $err)
+                    <div class="alert alert-danger alert-dismissible fade show mt-2 col-lg-6 m-auto" role="alert">
+                        <i class="fa-solid fa-triangle-exclamation me-3"></i>
+                        <strong>Error!</strong> {{ $err }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endforeach
+            @endif
             <form method="POST" action="{{ route('login') }}" class="col-lg-6 m-auto">
                 @csrf
                 <div class="my-3">
                     <label for="Email" class="input-label">Email</label>
                     <input type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus
                         autocomplete="username">
-                    @if ($errors->has('email'))
-                        <ul class="bg-danger">
-                            @foreach ($errors->get('email') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
                 <div class="my-3">
                     <label for="Password" class="input-label">Password</label>
                     <input type="password" class="form-control" value="{{ old('password') }}" name="password" required
                         autocomplete="current-password">
-                    @if ($errors->has('password'))
-                        <ul class="bg-danger">
-                            @foreach ($errors->get('password') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
                 <div class="my-3">
                     <label for="remember_me" class="input-label">Remember me</label>
                     <input type="checkbox" class="form-check-input" name="remember" id="remember_me">
-                    @if ($errors->has('remember'))
-                        <ul class="bg-danger">
-                            @foreach ($errors->get('remember') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
                 <div class="d-flex justify-content-between mt-3">
                     @if (Route::has('password.request'))
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,15 @@
     <section class="py-5 text-dark" style="background-color: #FEFAF6">
         <div class="container">
             <h1 class="text-center">Register</h1>
+            @if ($errors)
+                @foreach ($errors->all() as $err)
+                    <div class="alert alert-danger alert-dismissible fade show mt-2 col-lg-6 m-auto" role="alert">
+                        <i class="fa-solid fa-triangle-exclamation me-3"></i>
+                        <strong>Error!</strong> {{ $err }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endforeach
+            @endif
             <form method="POST" action="{{ route('register') }}" class="col-lg-6 m-auto">
                 @csrf
 
@@ -12,51 +21,23 @@
                     <label for="name" class="input-label">Name</label>
                     <input type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus
                         autocomplete="name">
-                    @if ($errors->has('name'))
-                        <ul class="">
-                            @foreach ($errors->get('name') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
                 <!-- Email Address -->
                 <div>
                     <label for="Email" class="input-label">Email</label>
                     <input type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
-                    @if ($errors->has('email'))
-                        <ul class="bg-danger">
-                            @foreach ($errors->get('email') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
 
                 <!-- Password -->
                 <div>
                     <label for="Password" class="input-label">Password</label>
                     <input type="password" class="form-control" name="password" required autocomplete="password">
-                    @if ($errors->has('password'))
-                        <ul class="bg-danger mt-3">
-                            @foreach ($errors->get('password') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
                 <!-- Confirm Password -->
                 <div>
                     <label for="password_confirmation" class="input-label">Confirm Password</label>
                     <input type="password" class="form-control" name="password_confirmation" required
                         autocomplete="new-password">
-                    @if ($errors->has('password'))
-                        <ul class="bg-danger">
-                            @foreach ($errors->get('password') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
                 </div>
 
                 <div class="d-flex justify-content-between mt-3">
